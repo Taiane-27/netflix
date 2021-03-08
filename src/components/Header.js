4a0e2d6f6ab86c2f5b386e61a0830c4b481fd5e7
Taiane Quirino <tayquirino27@gmail.com>
2021-03-08 14:31:32 -0300

    primeiro projeto com react

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default ({black}) => {
         <header className={black ? "black" : ''}>
             <div className="header--logo">
                 <a href="/">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" />
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" />
                 </a>
             </div>
             <div className="header--user">
